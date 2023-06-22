f162aae0692e6538c2b3ff8e5bb899023282cdb1
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-06-22 22:11:33 +0530

    created the foundation

--- QuizApp Foundation/pyquiz.py
+++ QuizApp Foundation/pyquiz.py
@@ -21,3 +21,32 @@ class QuizApp:
         print("(L): List quizzes")
         print("(T): Take a quiz")
         print("(E): Exit program")
+
+    def menu_error(self):
+        print("That is not a valid selection. Please try again.")
+    
+    def goodbye(self):
+        print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-")
+        print(f"Thank for using PyQuiz, {self.username}! ")
+        print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-")
+
+    def menu(self):
+        self.menu_header()
+
+        #Run untill the user select the exit selection
+
+        selection = ""
+
+    #This is the entry poin to the program
+
+    def run(self):
+
+        #execute the startup routine
+        self.startup()
+        #Start main program
+        self.main()
+
+if __name__ = "__main__":
+    app = QuizApp()
+    app.run()
+
