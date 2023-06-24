dac63664d3ca121c8d35e5426fc8eaaad21c6203
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-06-24 21:25:17 +0530

    making quizzers

---
+++ Basics/02/quiz.py
@@ -0,0 +1,26 @@
+
+class Quiz:
+    def __init__(self):
+        
+        self.name = ""
+        self.description = ""
+        self.questions = []
+        self.score = 0
+        self.correct_count = 0
+        self.total_points = 0  
+
+    def print_header(self):
+        print("\n\n*************************************")
+
+        #ToDo: print the quiz header
+
+        print("*****************************************\n")
+
+    def print_result(self):
+        print("*************************************")
+
+        print("*****************************************\n")
+
+    def take_quiz(self):
+
+        
