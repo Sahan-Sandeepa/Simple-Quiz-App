e0083914f788b11189b223f725fa0fb4bbc1f663
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-06-26 19:38:24 +0530

    modified the logic to ge the data from xml file

--- Basics/O3/quizparser.py
+++ Basics/O3/quizparser.py
@@ -59,6 +59,11 @@ class QuizParser(xml.sax.ContentHandler):
         elif tagname == "QuestionText":
             self._parse_state = QuizParserState.PARSE_QUEST_TEXT
             self._current_question.correct_answer = attrs["answer"]
+            
+        elif tagname == "Answer":
+            self._current_answer = Answer()
+            self._current_answer.name = attrs["name"]
+            self._parse_state = QuizParserState.PARSE_ANSWER
 
     def endElement(self, tagname):
 
@@ -70,11 +75,26 @@ class QuizParser(xml.sax.ContentHandler):
             self.new_quiz.questions.append(self._current_question)
             self._parse_state = QuizParserState.PARSE_QUIZ  
         #ToDo: process the rest of the tags
+        elif tagname == "QuestionText":
+            self._parse_state = QuizParserState.PARSE_QUESTION
+        elif tagname == "Answer":
+            self._current_question.answers.append(self._current_answer)
+            self._parse_state = QuizParserState.PARSE_QUESTION
 
     def characters(self, chars):
 
         if self._parse_state == QuizParserState.PARSE_DESCRIPTION:
             self.new_quiz.description += chars
+        elif self._parse_state == QuizParserState.PARSE_QUEST_TEXT:
+            self._current_question.text += chars
 
-
+if __name__ == "__main__":
+    app = QuizParser()
+    qz = app.parse_quiz("Quizzers/SampleQuiz.xml")
+    print(qz.name)
+    print(qz.description)
+    print(len(qz.questions))
+    print(qz.total_points)
+    for q in qz.questions:
+        print(q.text)
     
